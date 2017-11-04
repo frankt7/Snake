92ec93d62be6159d0dd67821d5eb1b15382b8c2c
frankt7 <fengmaot@buffalo.edu>
2017-11-04 18:23:13 -0400

    initial file commit

---
+++ src/snake/test/SnakeTest.java
@@ -0,0 +1,14 @@
+package snake.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
