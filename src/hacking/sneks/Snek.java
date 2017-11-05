1cc99c8dfa399a76069ae76f3522fc6dfdb7ff50
frankt7 <fengmaot@buffalo.edu>
2017-11-05 07:31:32 -0500

    update comments

--- src/hacking/sneks/Snek.java
+++ src/hacking/sneks/Snek.java
@@ -8,18 +8,20 @@ package hacking.sneks;
  */
 public interface Snek {
 	/**
-	 * Adds an element to this list.
+	 * Adds an element to the front of this list.
 	 * @param elem - the element to be added
 	 */
 	public <E> void add(E elem);
 	
 	/**
 	 * Removes the first occurrence of element which is equal to the supplied argument from
-	 * the collection. If the an element is removed from this collection, {@code true} is returned;
+	 * the list. If the an element is removed from this list, {@code true} is returned;
 	 * otherwise {@code false} is returned. {@link Object#equals(Object)} must be
-	 * overriden properly for this method to work unless the default action is desired.
+	 * overriden properly for this method to work.
 	 * @param elem - the element to be removed
 	 * @return {@code true} if an element is remove; {@code false} otherwise.
 	 */
 	public <E> boolean remove(E elem);
+	
+	
 }
